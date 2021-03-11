8b291dab54927e6fea3364afdc2cddbb14eb938d
jsandov <jfssandoval92@gmail.com>
2021-03-11 13:39:05 -0800

    Rendered Person component onto main page with props

--- prop-it-up-assignment/src/App.js
+++ prop-it-up-assignment/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./components/person-component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person lastName="Doe" firstName="Jane" age="48" hairColor="Black" />
+      <Person lastName="Smith" firstName="John" age="88" hairColor="Brown" />
+      <Person
+        lastName="Fillmore"
+        firstName="Millard"
+        age="50"
+        hairColor="Brown"
+      />
+      <Person lastName="Smith" firstName="Maria" age="62" hairColor="Brown" />
     </div>
   );
 }
